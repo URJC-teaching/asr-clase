7733ad793e0950cd11c32f298898fd4efdf1b511
Francisco Martín Rico <fmrico@gmail.com>
2025-03-17 20:53:19 +0100

    Fixed HSV Filter crash when distorsion model is unknown
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- sensors/camera/src/camera/HSVFilterNode.cpp
+++ sensors/camera/src/camera/HSVFilterNode.cpp
@@ -172,7 +172,6 @@ HSVFilterNode::image_callback(const sensor_msgs::msg::Image::ConstSharedPtr & im
   cv::Point2d point = get_detected_center(image_filtered);
 
   if (model_->cameraInfo().distortion_model != "") {
-    std::cerr << "No pases!!!" << std::endl;
     auto [yaw, pitch] = get_detected_angles(point, model_);
 
     RCLCPP_INFO(
