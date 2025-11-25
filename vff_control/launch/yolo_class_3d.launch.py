10b420270b65f2cb0e81e40ffcba3a5f5b070d7c
roi <rodrigo.perez@urjc.es>
2025-11-25 22:51:29 +0100

    minor fix

--- vff_control/launch/yolo_class_3d.launch.py
+++ vff_control/launch/yolo_class_3d.launch.py
@@ -14,8 +14,8 @@ def generate_launch_description():
             name='yolo_class_detector_node_3d',
             output='screen',
             parameters=[{
-                'target_class': 'chair',
-                'base_frame': 'oak-d_frame'
+                'target_class': 'cup',
+                'base_frame': 'base_footprint'
             }],
             remappings=[
             ('/input_detection_3d', '/detections_3d'),
